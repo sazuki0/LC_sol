de46d5666c31dc7e4e52c74c1c192e6d2682b66f
Haochen Zhang <zhcharrt@gmail.com>
2024-08-23 12:02:57 -0400

    Update LC3_longest_substr_no_repeat.cpp

--- LC3_longest_substr_no_repeat.cpp
+++ LC3_longest_substr_no_repeat.cpp
@@ -1,6 +1,14 @@
+/*
+Problem: Longest Substring Without Repeating Characters
+
+Given a string s, find the length of the longest substring without repeating characters.
+*/    
+
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+
+        //Use map and front back iterators to process characters, calculate size by elements inside map
         
         std::unordered_map<char, int> chars;
         int front = 0, back = 0;
